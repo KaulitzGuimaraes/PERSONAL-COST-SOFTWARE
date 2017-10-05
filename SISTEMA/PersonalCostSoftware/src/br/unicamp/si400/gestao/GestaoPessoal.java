b3061d4e6902b75a3699622320f33094acbcca08
KaulitzGuimaraes <kaulitzguimaraes@hotmail.com>
2017-10-05 16:02:15 -0300

    models terminados

---
+++ SISTEMA/PersonalCostSoftware/src/br/unicamp/si400/gestao/GestaoPessoal.java
@@ -0,0 +1,80 @@
+/*
+ * Faculdade de Tecnologia da UNICAMP 
+ * SI400 - Programação Orientada a Objetos II (JAVA)
+ *  author - Kaulitz Guimaraes RA: 188530
+ */
+package br.unicamp.si400.gestao;
+
+/**
+ *
+ * @author Kaulitz
+ */
+public class GestaoPessoal {
+
+    private  Object resultado;
+
+    public GestaoPessoal() {
+        this.resultado = 0;
+    }
+
+    public Double valorRestante(double minuendo, double subtraendo) {
+
+        this.resultado = (Double)minuendo - subtraendo;
+
+        return (Double) this.resultado;
+    }
+    
+    public String valorRestantePorcentagem(double minuendo, double subtraendo) {
+
+        this.resultado = (String)Double.toString((((minuendo - subtraendo)/minuendo)*100)) +"%";
+        
+        return (String) this.resultado;
+        
+        
+    }
+    
+    public Double valorSomado(double minuendo, double subtraendo) {
+
+        this.resultado = minuendo + subtraendo;
+
+        return (Double) this.resultado;
+    }
+    
+    public Double mediaValores(double[] data){
+        double buffer =0;
+        for (double el : data){
+            buffer += el;
+            
+        }
+        this.resultado = (Double) buffer/(data.length);
+        return (Double) this.resultado;
+        
+    }
+    
+    public Double varianciaValores(double[] data){
+        double media = mediaValores(data);
+        double buffer =0;
+        for (double el : data){
+            buffer += Math.pow(el, 2);
+            
+        }
+        
+        this.resultado = (Double) (buffer/ data.length) - Math.pow(media, 2) ;
+        return (Double) this.resultado;
+        
+    }
+    
+    
+    public Double desvioPadraoValores(double[] data){
+        double variancia = varianciaValores(data);
+        
+        
+        this.resultado = (Double) Math.sqrt(variancia) ;
+        return (Double) this.resultado;
+        
+    }
+    public Object getResultado() {
+        return resultado;
+    }
+
+}
